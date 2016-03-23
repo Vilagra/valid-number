df0bb8874ebd8e912b44aaae21dad87e75a49d0b
konstantin-vrfl <vrfl@mail.ru>
2016-03-24 00:53:35 +0200

    remarks

--- src/test/java/CheckNumberTest.java
+++ src/test/java/CheckNumberTest.java
@@ -8,60 +8,65 @@ import static org.junit.Assert.*;
  */
 public class CheckNumberTest {
 
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
     @Test
-    public void testCheckOnValidity() throws Exception {
-        assertEquals(true ,CheckNumber.checkOnValidity("+380501234567"));
+    public void checkOnValidityTest1() {
+        assertTrue(CheckNumber.checkOnValidity("+380501234567"));
     }
     @Test
-    public void testCheckOnValidity1() throws Exception {
-        assertEquals(true ,CheckNumber.checkOnValidity("+38(050)1234567"));
+    public void checkOnValidityTest2() {
+        assertTrue(CheckNumber.checkOnValidity("+38(050)1234567"));
     }
     @Test
-    public void testCheckOnValidity2() throws Exception {
-        assertEquals(true ,CheckNumber.checkOnValidity("+380-5012-34567"));
+    public void checkOnValidityTest3(){
+        assertTrue(CheckNumber.checkOnValidity("+380-5012-34567"));
     }
     @Test
-    public void testCheckOnValidity4() throws Exception {
-        assertEquals(true ,CheckNumber.checkOnValidity("0501234567"));
+    public void checkOnValidityTest4(){
+        assertTrue(CheckNumber.checkOnValidity("0501234567"));
     }
     @Test
-    public void testCheckOnValidity5() throws Exception {
-        assertEquals(true ,CheckNumber.checkOnValidity("050123-4567"));
+    public void checkOnValidityTest5(){
+        assertTrue(CheckNumber.checkOnValidity("050123-4567"));
     }
     @Test
-    public void testCheckOnValidity6() throws Exception {
-        assertEquals(false ,CheckNumber.checkOnValidity("+38)050(1234567"));
+    public void checkOnValidityTest6(){
+        assertFalse(CheckNumber.checkOnValidity("+38)050(1234567"));
     }
     @Test
-    public void testCheckOnValidity7() throws Exception {
-        assertEquals(false ,CheckNumber.checkOnValidity("+38(050)1-23-45-6-7"));
+    public void checkOnValidityTest7(){
+        assertFalse(CheckNumber.checkOnValidity("+38(050)1-23-45-6-7"));
     }
     @Test
-    public void testCheckOnValidity8() throws Exception {
-        assertEquals(false ,CheckNumber.checkOnValidity("050xxx4567"));
+    public void checkOnValidityTest8(){
+        assertFalse(CheckNumber.checkOnValidity("050xxx4567"));
     }
     @Test
-    public void testCheckOnValidity9() throws Exception {
-        assertEquals(false ,CheckNumber.checkOnValidity("050123456"));
+    public void checkOnValidityTest9(){
+        assertFalse(CheckNumber.checkOnValidity("050123456"));
     }
     @Test
-    public void testCheckOnValidity10() throws Exception {
-        assertEquals(false ,CheckNumber.checkOnValidity("(0)501234567"));
+    public void checkOnValidityTest10(){
+        assertFalse(CheckNumber.checkOnValidity("(0)501234567"));
     }
     @Test
-    public void testCheckOnValidity11() throws Exception {
-        assertEquals(true ,CheckNumber.checkOnValidity("(050)1234567"));
+    public void checkOnValidityTest11(){
+        assertTrue(CheckNumber.checkOnValidity("(050)1234567"));
     }
     @Test
-    public void testCheckOnValidity12() throws Exception {
-        assertEquals(true ,CheckNumber.checkOnValidity("(050)12-34-567"));
+    public void checkOnValidityTest12(){
+        assertTrue(CheckNumber.checkOnValidity("(050)12-34-567"));
     }
-
-
+    @Test
+    public void checkOnValidityTest13(){
+        assertFalse(CheckNumber.checkOnValidity(null));
+    }
+    @Test
+    public void checkOnValidityTest14( ){
+        assertFalse(CheckNumber.checkOnValidity("38(123)4567890")); //3 only with "+"
+    }
+    @Test
+    public void checkOnValidityTest15( ){
+        assertFalse(CheckNumber.checkOnValidity("8(123)4567890")); //8 only with +d8
+    }
 
 }
